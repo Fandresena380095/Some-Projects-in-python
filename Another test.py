c20511da8c484e7b4f954d4abad35886c46f63b6
Ny Avo <Jeandelacroix38@gmail.com>
2021-07-11 19:51:05 +0300

    some reversed list function added

--- Another test.py
+++ Another test.py
@@ -15,4 +15,19 @@ num3 = [55456,87,975757,34,987,65546565,98758585,986875754653642,22,2323223,2323
 num3.sort()
 print(num3)
 
+li = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+
+def reverse_this_list(selected_list):
+    x = len(selected_list)-1
+    y = 0
+    reversed_list = []
+    while y<=x:
+        reversed_list.append(selected_list[x-y])
+        y += 1
+    return reversed_list
+
+print(reverse_this_list(li))
+
+
 
